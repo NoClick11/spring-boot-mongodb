b4898a9603616dfbfc14841c086e69afc1a70010
NoClick11 <bahamonde.boullosa@gmail.com>
2025-05-15 17:52:36 -0300

    MongoDB connected to repository and service

---
+++ src/main/java/com/manuelneto/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.manuelneto.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.manuelneto.workshopmongo.domain.User;
+import com.manuelneto.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
