c39b2e0e1f3d35d535b425ecde1bc291a1d12b4f
NoClick11 <bahamonde.boullosa@hotmail.com>
2025-05-15 19:43:03 -0300

    User insertion with post

--- src/main/java/com/manuelneto/workshopmongo/services/UserService.java
+++ src/main/java/com/manuelneto/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.manuelneto.workshopmongo.domain.User;
+import com.manuelneto.workshopmongo.dto.UserDTO;
 import com.manuelneto.workshopmongo.repository.UserRepository;
 import com.manuelneto.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,15 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
+	}
+	
+	public User insert(User obj){
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getEmail(), objDto.getName());
+	}
+	
+	
 }
