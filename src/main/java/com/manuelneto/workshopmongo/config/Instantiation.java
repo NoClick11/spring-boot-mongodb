5fd2bb06b164d73d97616eff39f349465e61742c
NoClick11 <bahamonde.boullosa@gmail.com>
2025-05-15 22:53:38 -0300

    Referencing user posts

--- src/main/java/com/manuelneto/workshopmongo/config/Instantiation.java
+++ src/main/java/com/manuelneto/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.manuelneto.workshopmongo.domain.Post;
 import com.manuelneto.workshopmongo.domain.User;
+import com.manuelneto.workshopmongo.dto.AuthorDTO;
 import com.manuelneto.workshopmongo.repository.PostRepository;
 import com.manuelneto.workshopmongo.repository.UserRepository;
 
@@ -35,11 +36,15 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
