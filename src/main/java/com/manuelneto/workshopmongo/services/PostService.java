e188c82dc1e95b50dc879b7b2a9dda5750e96b4f
NoClick11 <bahamonde.boullosa@hotmail.com>
2025-05-26 00:17:45 -0300

    Get a post by id

---
+++ src/main/java/com/manuelneto/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.manuelneto.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.manuelneto.workshopmongo.domain.Post;
+import com.manuelneto.workshopmongo.domain.User;
+import com.manuelneto.workshopmongo.repository.PostRepository;
+import com.manuelneto.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
