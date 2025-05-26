3537250a34e24fc9a599f80dcc01871e6a860671
NoClick11 <bahamonde.boullosa@hotmail.com>
2025-05-26 16:08:08 -0300

    Multi-criteria query

--- src/main/java/com/manuelneto/workshopmongo/services/PostService.java
+++ src/main/java/com/manuelneto/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.manuelneto.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.manuelneto.workshopmongo.domain.Post;
-import com.manuelneto.workshopmongo.domain.User;
 import com.manuelneto.workshopmongo.repository.PostRepository;
 import com.manuelneto.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +24,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
